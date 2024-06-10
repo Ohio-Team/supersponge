9f2efbccb1e715f0695b3c112ade77063e4da6ac
flowcheer <lolxdwhy1@outlook.com>
2024-06-10 18:37:18 -0500

    bob 3d

--- Spongebob.gd
+++ Spongebob.gd
@@ -1,8 +1,9 @@
 extends CharacterBody3D
 
 
-const SPEED = 24.0
-const JUMP_VELOCITY = 4.5
+@export var SPEED = 24.0
+@onready var camera : ThirdPersonCamera = $ThirdPersonCamera
+const JUMP_VELOCITY = 24.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -20,7 +21,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (camera.get_front_direction() * (transform.basis * Vector3(input_dir.x, 0, input_dir.y))).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
