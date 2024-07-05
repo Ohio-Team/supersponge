bd307edfe788ad1b0c40742326dee0f8b9669c20
flowcheer <lolxdwhy1@outlook.com>
2024-07-04 21:09:29 -0500

    i think i fucked something up last commit

--- scripts/cutscene_handler.gd
+++ scripts/cutscene_handler.gd
@@ -5,12 +5,15 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	MusicPlayer.stop_song()
-
+	$VideoStreamPlayer.connect("finished", gotonext)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+func gotonext():
+	get_tree().change_scene_to_packed(after_cutscene)
+
 func _unhandled_key_input(event):
 	if event.is_pressed():
-		get_tree().change_scene_to_packed(after_cutscene)
+		gotonext()
