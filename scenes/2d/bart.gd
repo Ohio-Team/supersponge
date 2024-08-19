a4d20fb117dba6214f9bbc787fe5440508478ffe
flowcheer <lolxdwhy1@outlook.com>
2024-08-18 21:52:11 -0500

    SCARY. BASH.

--- scenes/2d/bart.gd
+++ scenes/2d/bart.gd
@@ -1,17 +1,22 @@
 extends Area2D
 
+var speed : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	speed = Vector2(randi_range(-10,10),randi_range(-10,10))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position += speed
 
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		print("bart bashed.")
 		Singleton.do_explosion(position)
+		get_parent().get_parent().bartcounter += 1
 		queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	speed = -speed
--- scenes/2d/bart_bash.gd
+++ scenes/2d/bart_bash.gd
@@ -1,12 +1,18 @@
 extends Node2D
-
+var bartcounter : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	pass # Replace with function body.
+	MusicPlayer.play_song("res://assets/music/bartbash.mp3")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if bartcounter == 32:
+		$Window.show()
+		MusicPlayer.stop_song()
+		await Singleton.wait(5)
+		$Window/Camera2D/Window.show()
+		await Singleton.wait(2)
+		get_tree().quit()
