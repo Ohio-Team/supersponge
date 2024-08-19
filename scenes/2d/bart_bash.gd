5de7a6ff2c4c221fdc326af193b62b4597518327
flowcheer <lolxdwhy1@outlook.com>
2024-08-18 22:09:09 -0500

    added more creepiness

--- scenes/2d/bart_bash.gd
+++ scenes/2d/bart_bash.gd
@@ -1,6 +1,6 @@
 extends Node2D
 var bartcounter : int = 0
-
+var increasingthing : float = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -11,8 +11,12 @@ func _ready():
 func _process(delta: float) -> void:
 	if bartcounter == 32:
 		$Window.show()
+		$Window.position = lerp(Vector2(326,74), Vector2(300,74), increasingthing)
+		if increasingthing != 1:
+			increasingthing += 0.1
 		MusicPlayer.stop_song()
-		await Singleton.wait(5)
-		$Window/Camera2D/Window.show()
-		await Singleton.wait(2)
-		get_tree().quit()
+		if $Window.position == Vector2i(300,74):
+			await Singleton.wait(5)
+			$Window/Camera2D/Window.show()
+			await Singleton.wait(2)
+			get_tree().quit()
