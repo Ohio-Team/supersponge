8e05824558750f72f36a81b6d61442be0e2e98c4
flowcheer <lolxdwhy1@outlook.com>
2024-08-20 18:25:08 -0500

    ok

--- scripts/bart_bash.gd
+++ scripts/bart_bash.gd
@@ -1,6 +1,5 @@
 extends Node2D
 var bartcounter : int = 0
-var increasingthing : float = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -11,12 +10,11 @@ func _ready():
 func _process(delta: float) -> void:
 	if bartcounter == 32:
 		$Window.show()
-		$Window.position = lerp(Vector2(326,74), Vector2(300,74), increasingthing)
-		if increasingthing != 1:
-			increasingthing += 0.1
+		$Window.position = lerp(Vector2($Window.position), Vector2(-10,-5), 0.1)
 		MusicPlayer.stop_song()
-		if $Window.position == Vector2i(300,74):
+		if $Window.position == Vector2i(0,0):
 			await Singleton.wait(5)
 			$Window/Camera2D/Window.show()
 			await Singleton.wait(2)
-			get_tree().quit()
+			$Window/Camera2D/Window.hide()
+			$AudioStreamPlayer.play()
