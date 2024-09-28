833c6d4506f11cf88efeb8faa3a7775b1016f3db
Flowcheer <lolxdwhy1@outlook.com>
2024-09-28 15:11:26 -0500

    Ohio Sigma Android

--- scripts/car.gd
+++ scripts/car.gd
@@ -10,8 +10,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	steering = move_toward(steering, Input.get_axis("right","left") * 0.8, delta * 2.5)
 	engine_force = Input.get_axis("back","front") * 200
+	if OS.get_name() == "Android":
+		steering = move_toward(steering, Ui.posVector.x * 0.8, delta * 2.5)
+		engine_force = Ui.posVector.y * 200
 	if position.y < -1 or Singleton.fuel < 0:
 		get_tree().reload_current_scene()
 		
-	if Input.is_action_pressed("front") or Input.is_action_pressed("back"):
+	if Input.is_action_pressed("front") or Input.is_action_pressed("back") or (Ui.joystick.posVector.y > 0.1 or Ui.joystick.posVector.y < 0):
 		Singleton.fuel -= delta * 10
