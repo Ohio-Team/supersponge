16bb6b605ab4705b2e8ea7d7432b317ed598128d
Flowcheer <lolxdwhy1@outlook.com>
2024-09-28 15:49:30 -0500

    updated intro code

--- scripts/level4intro.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-func _ready() -> void:
-	MusicPlayer.stop_song()
-
-func _on_audio_stream_player_finished() -> void:
-	get_tree().change_scene_to_file("res://scenes/3d/level_4.tscn")
