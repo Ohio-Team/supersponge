a0cd190463a65bece51f5b3c4e1a7dfa9e477484
juanenchilada670 <cacapopofuchilandia@gmail.com>
2024-10-21 19:10:35 -0700

    Work on the jail cell

--- scripts/spongebobandclick.gd
+++ scripts/spongebobandclick.gd
@@ -13,4 +13,5 @@ func _physics_process(delta: float) -> void:
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		BMOD.play_sfx(preload("res://assets/sfx/walk.tres"))
 		move_and_collide(get_local_mouse_position())
