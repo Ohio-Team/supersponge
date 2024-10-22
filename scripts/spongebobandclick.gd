d816bb473852b5a5b0975543af699f0951e93431
flowcheer <lolxdwhy1@outlook.com>
2024-10-21 20:37:58 -0500

    point and click progress

---
+++ scripts/spongebobandclick.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+var hasclip:bool = false
+var target:Vector2
+
+
+func _ready() -> void:
+	set_process_unhandled_input(true)
+
+func _physics_process(delta: float) -> void:
+	pass
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		move_and_collide(get_local_mouse_position())
