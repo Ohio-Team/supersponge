6590aae5874af7e309e9d9570d4df9fec49b273c
juanenchilada670 <cacapopofuchilandia@gmail.com>
2024-12-03 21:19:31 -0800

    Token improvements

--- scenes/2d/token.gd
+++ scenes/2d/token.gd
@@ -1,21 +1,22 @@
 extends Area2D
 @onready var player = get_tree().get_first_node_in_group("Player")
+@export var image:Texture
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Texture.texture = image
 	
 	
 func _on_body_entered(body):
 	if body == player:
 		print("token collected")
-		BMOD.play_sfx(preload("res://assets/sfx/fuel.tres"))
+		BMOD.play_sfx(preload("res://assets/sfx/token.tres"))
 		queue_free()
 
 
-@export var frequency = 6: 
+@export var frequency: int = 6: 
 	set(float): 1000
-@export var amplitude = 50: 
+@export var amplitude: int = 50: 
 	set(float): 1 
 var time = 0
 
@@ -23,3 +24,5 @@ func _physics_process(delta: float) -> void:
 	time += delta
 	var movement = cos(time*frequency)*amplitude
 	position.y+=movement * delta
+	
+	
