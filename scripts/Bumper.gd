c0dbec32871230c8e1926a369e83c8ec9ac4cce9
flowcheer <79985497+Flowcheer@users.noreply.github.com>
2024-12-16 18:20:01 -0500

    added token

--- scripts/Bumper.gd
+++ scripts/Bumper.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @onready var player = get_tree().get_first_node_in_group("Player")
-
+@export var bounce_force:int = -1000
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,4 +15,4 @@ func _process(delta):
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		BMOD.play_sfx(preload("res://assets/sfx/spring.tres"))
-		body.velocity.y = -1000
+		body.velocity.y = bounce_force
