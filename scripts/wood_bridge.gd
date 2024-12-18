1ff6155ae6885ef610da6cd0294a5cd2a78392b2
flowcheer <79985497+Flowcheer@users.noreply.github.com>
2024-12-18 17:55:44 -0500

    pipe test

--- scripts/wood_bridge.gd
+++ scripts/wood_bridge.gd
@@ -10,14 +10,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func sound_play():
-	BMOD.play_sfx(preload("res://assets/sfx/woodbreak.tres"))
-
 func _on_hit_body_entered(body: Node2D) -> void:
 	await Singleton.wait(0.5)
 	$CollisionShape2D.disabled = true
 	$WoodB04.visible = false
-	sound_play()
+	BMOD.play_sfx_2d(preload("res://assets/sfx/woodbreak.tres"),position)
 	await Singleton.wait(3)
 	$CollisionShape2D.disabled = false
 	$WoodB04.visible = true
