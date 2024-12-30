a9d41aa251bb1c021eecba77d68d4e61b03c01ae
juanenchilada670 <79539842+juanenchilada670@users.noreply.github.com>
2024-12-29 16:39:31 -0800

    Skatebob control improvements
    You can now use the keyboard/gamepad to control him

--- scripts/skate.gd
+++ scripts/skate.gd
@@ -6,7 +6,7 @@ var direction := 1
 const JUMP_VELOCITY = -400.0
 
 func _ready():
-	Ui.create_dialog("This Ye again click on bob to gain speed and press right click to jump. Now get out of here and do a little... [b]runaway.[b]","kanye")
+	Ui.create_dialog("This Ye again press ATTACK to gain speed and press JUMP to jump. Now get out of here and do a little... [b]runaway.[b]","kanye")
 
 func _physics_process(delta: float) -> void:
 	$Camera2D.zoom = lerp($Camera2D.zoom,Vector2(1,1),delta * 3)
@@ -15,7 +15,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		BMOD.play_sfx(preload("res://assets/sfx/jump.tres"))
 		velocity.y = JUMP_VELOCITY
 
@@ -31,8 +31,8 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		SPEED += 10
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT or Input.is_action_just_pressed("attack"):
+		SPEED += 15
 		$AnimatedSprite2D.play("momentum")
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and is_on_floor():
 		velocity.y = JUMP_VELOCITY
