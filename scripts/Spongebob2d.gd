a946695ecc9a500a276e8f38212d6bdda0d17db5
flowcheer <79985497+Flowcheer@users.noreply.github.com>
2025-02-12 14:57:52 -0500

    Update Spongebob2d.gd

--- scripts/Spongebob2d.gd
+++ scripts/Spongebob2d.gd
@@ -139,5 +139,5 @@ func _on_timer_timeout() -> void:
 
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and state != "dying":
 		state = "hurt"
